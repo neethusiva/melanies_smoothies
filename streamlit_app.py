459711fbbe9d48a3200d0525b17eb9f0de15f7e9
Neethu Sivaraj Adiyoli <97852827+neethusiva@users.noreply.github.com>
2025-01-03 18:46:15 +0530

    Update streamlit_app.py
    Added requests python library code

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         success_msg = 'Your Smoothie is ordered, ' + name_on_order + '!'
         st.success(success_msg, icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
